bae8a6b3188caedb7d449f79ea61321539e4f990
unknown <knelia1989@gmail.com>
2023-08-07 10:09:52 -0500

    Cambios en camelCase, ; e iteraciones.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,36 @@
 import { example } from './data.js';
-// import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
 
 console.log(example, data);
+
+const rootElement = document.getElementById("root");
+
+function pokemonCards(){
+  const pokemonArray = data.pokemon;//data.pokemon accede a la matriz del objeto pokemon y se en la var pokemon Array 
+  pokemonArray.forEach(pokemonInfo=>{
+    //Crear elementos HTML para guardar la informacion de cada pokemon 
+    const pokemonCard = document.createElement('div'); //HTML 
+    pokemonCard.classList.add('pokemon-container'); //CSS
+
+    const pokemonImg = document.createElement('img'); //HTML
+    pokemonImg.classList.add('pokemon-img'); //CSS
+    pokemonImg.src = pokemonInfo.img;
+    pokemonCard.appendChild(pokemonImg);
+
+    const pokemonName = document.createElement('h3');//HTML
+    pokemonName.classList.add('pokemon-name'); //CSS
+    pokemonName.textContent = pokemonInfo.name;
+    pokemonCard.appendChild(pokemonName);
+
+    const pokemonNumber = document.createElement('p');
+    pokemonNumber.classList.add('pokemon-number');
+    pokemonNumber.textContent = `#${pokemonInfo.num}`;
+    pokemonCard.appendChild(pokemonNumber);
+
+    rootElement.appendChild(pokemonCard);
+      
+    console.log(pokemonCard);
+      
+  });
+}
+document.addEventListener('DOMContentLoaded', pokemonCards);

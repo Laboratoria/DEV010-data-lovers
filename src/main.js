e88ae69bd4ba128f3740e09862dfafc1fba00ca2
Gaby Noriega <gabriela.noriega@alumnos.udg.mx>
2023-08-09 07:08:46 -0400

    Visualizacion de las cartas

--- src/main.js
+++ src/main.js
@@ -1,6 +1,27 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+// Importa los datos de los Pokémon
+ 
+import pokemonData from './data/pokemon/pokemon.js';
 
-console.log(example, data);
+const pokemons = pokemonData.pokemon; // Accede a la propiedad "pokemon"
+
+for (const pokemon of pokemons) {
+    const card = document.createElement('div');
+    card.classList.add('card');
+
+    card.innerHTML = `  
+        <img src="${pokemon.img}" alt="${pokemon.name}"> 
+        <p>#${pokemon.num}</p>
+        <h2> ${pokemon.name}</h2>  
+        <p> ${pokemon['pokemon-rarity']}</p>
+        <p> ${pokemon.type}</p>
+        <div class="additional-info">
+        <p> About: ${pokemon.about}</p>
+        <p> Evolution: ${pokemon.evolution}</p>
+        <p> ${pokemon.generation.num}</p>
+        <p> Quick move: ${pokemon['special-attack']}</p>
+        <p> Resistant: ${pokemon.resistant}</p>
+        </div>
+    `;
+
+    document.body.appendChild(card);
+}

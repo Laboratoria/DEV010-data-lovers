8b5a4a21e21b90b0e6cade40f60a2dd6c5c13a63
Gaby Noriega <gabriela.noriega@alumnos.udg.mx>
2023-08-09 08:14:51 -0400

    MODIFICACION DEL BOTON

--- src/main.js
+++ src/main.js
@@ -5,16 +5,16 @@ import pokemonData from './data/pokemon/pokemon.js';
 const pokemons = pokemonData.pokemon; // Accede a la propiedad "pokemon"
 
 for (const pokemon of pokemons) {
-    const card = document.createElement('div');
+    const card = document.createElement('div'); // For recorre el arreglo pokemons, crea un nuevo elemento <div> para cada elemento en el arreglo y le añade la clase card. 
     card.classList.add('card');
-
+//Informacion que se mostrara de cada Tarjeta 
     card.innerHTML = `  
         <img src="${pokemon.img}" alt="${pokemon.name}"> 
         <p>#${pokemon.num}</p>
         <h2> ${pokemon.name}</h2>  
+        <div class="additional-info"> 
         <p> ${pokemon['pokemon-rarity']}</p>
         <p> ${pokemon.type}</p>
-        <div class="additional-info">
         <p> About: ${pokemon.about}</p>
         <p> Evolution: ${pokemon.evolution}</p>
         <p> ${pokemon.generation.num}</p>
@@ -23,5 +23,5 @@ for (const pokemon of pokemons) {
         </div>
     `;
 
-    document.body.appendChild(card);
+    document.body.appendChild(card); //Agrega el elemento card como un hijo del elemento body, para que el elemento card se muestre en la página, dentro del contenido principal.
 }

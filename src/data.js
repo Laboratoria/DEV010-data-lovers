2d734bcfec97b2d55761b4a9347330828d51d90c
Ale77$ <encuadernacionyali@gmail.com>
2023-08-10 17:12:20 -0600

    Agrega cambios en data.js y html

--- src/data.js
+++ src/data.js
@@ -1,17 +1,57 @@
 // estas funciones son de ejemplo
 
 export const example = () => {
-
-
-
-
-
-  
   return 'example';
 };
 
 export const anotherExample = () => {
   return 'OMG';
 };
+import data from './data/pokemon/pokemon.js';
 
+//Funcion busqueda de nombre o numero
+
+function busquedaNomNum (){
+  const inputElement = document.getElementById("input");
+  const btnBuscar = document.getElementById("btn-buscar");
+
+  const recibeNomNum = inputElement.value;
+
+  if(recibeNomNum === "string")
+
+}
+
+btnBuscar.addEventListener("click", )
+
+
+
+
+
+
+
+
+//Llama elementos del DOM
+/*const elementosSelect = document.getElementById("types");
+const btnBuscar = document.getElementById("btn-buscarType");
+const pokemonList = data.pokemon.pokemon
+
+//Funcion de filtrado
+function filtrarPorTipo(pokemonType) {
+  return pokemonList.filter(pokemon => pokemon.type.includes(pokemonType));
+
+}
+
+//Agregar un evento al boton de busqueda 
+
+btnBuscar.addEventListener("click", () => {
+  const tipoSeleccionado = elementosSelect.value;
+  const pokemonesFiltrados = filtrarPorTipo(tipoSeleccionado);
+  actualizarTarjetas(pokemonesFiltrados)
+
+  function actualizarTarjetas (pokemones){
+    const rootElement = document.getElementById("root");
+    rootElement.innerHTML = "";
+
+  }
+})*/
 
--- src/main.js
+++ src/main.js
@@ -1,15 +1,12 @@
-import { example } from './data.js';
+
 import data from './data/pokemon/pokemon.js';
 
-console.log(example, data);
-
 const rootElement = document.getElementById("root");
-
 function pokemonCards(){
-  const pokemonArray = data.pokemon;//data.pokemon accede a la matriz del objeto pokemon y se en la var pokemon Array 
+  const pokemonArray = data.pokemon;//data.pokemon accede a la matriz del objeto pokemon y se en la var pokemon Array
   pokemonArray.forEach(pokemonInfo=>{
-    //Crear elementos HTML para guardar la informacion de cada pokemon 
-    const pokemonCard = document.createElement('div'); //HTML 
+    //Crear elementos HTML para guardar la informacion de cada pokemon
+    const pokemonCard = document.createElement('div'); //HTML
     pokemonCard.classList.add('pokemon-container'); //CSS
 
     const pokemonImg = document.createElement('img'); //HTML
@@ -26,10 +23,9 @@ function pokemonCards(){
     pokemonNumber.classList.add('pokemon-number');
     pokemonNumber.textContent = `#${pokemonInfo.num}`;
     pokemonCard.appendChild(pokemonNumber);
-
+    
     rootElement.appendChild(pokemonCard);
     console.log(pokemonCard);
-      
   });
 }
 document.addEventListener('DOMContentLoaded', pokemonCards);

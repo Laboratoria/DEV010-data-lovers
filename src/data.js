01bdd832f64275cf57f8e3bfd99f00b349143db1
Ale77$ <encuadernacionyali@gmail.com>
2023-08-11 16:47:30 -0600

    Filtra los pokemones por tipo y realiza la busqueda por nombre

--- src/data.js
+++ src/data.js
@@ -1,5 +1,7 @@
 // estas funciones son de ejemplo
 
+
+
 export const example = () => {
   return 'example';
 };
@@ -7,51 +9,24 @@ export const example = () => {
 export const anotherExample = () => {
   return 'OMG';
 };
-import data from './data/pokemon/pokemon.js';
+
 
 //Funcion busqueda de nombre o numero
 
-function busquedaNomNum (){
-  const inputElement = document.getElementById("input");
-  const btnBuscar = document.getElementById("btn-buscar");
-
-  const recibeNomNum = inputElement.value;
-
-  if(recibeNomNum === "string")
+export function busquedaNomNum (data, name, num){
+  return data.pokemon.find(pokemon => {
+    return pokemon.name.toLowerCase() === name.toLowerCase() || pokemon.num === num;
+  
+  });
 
 }
 
-btnBuscar.addEventListener("click", )
 
-
-
-
-
-
-
-
-//Llama elementos del DOM
-/*const elementosSelect = document.getElementById("types");
-const btnBuscar = document.getElementById("btn-buscarType");
-const pokemonList = data.pokemon.pokemon
-
-//Funcion de filtrado
-function filtrarPorTipo(pokemonType) {
-  return pokemonList.filter(pokemon => pokemon.type.includes(pokemonType));
-
+export function filtrarPorTipo (data, condition){
+  return data.pokemon.filter(pokemon => {
+    return condition(pokemon); 
+  });
 }
 
-//Agregar un evento al boton de busqueda 
 
-btnBuscar.addEventListener("click", () => {
-  const tipoSeleccionado = elementosSelect.value;
-  const pokemonesFiltrados = filtrarPorTipo(tipoSeleccionado);
-  actualizarTarjetas(pokemonesFiltrados)
-
-  function actualizarTarjetas (pokemones){
-    const rootElement = document.getElementById("root");
-    rootElement.innerHTML = "";
-
-  }
-})*/
 
--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 
 import data from './data/pokemon/pokemon.js';
+import {busquedaNomNum, filtrarPorTipo} from './data.js';
+
+
 
 const rootElement = document.getElementById("root");
-function pokemonCards(){
-  const pokemonArray = data.pokemon;//data.pokemon accede a la matriz del objeto pokemon y se en la var pokemon Array
+function pokemonCards(pokemonArray){
+  //const pokemonArray = data.pokemon;//data.pokemon accede a la matriz del objeto pokemon y se en la var pokemon Array
   pokemonArray.forEach(pokemonInfo=>{
     //Crear elementos HTML para guardar la informacion de cada pokemon
     const pokemonCard = document.createElement('div'); //HTML
@@ -28,4 +31,61 @@ function pokemonCards(){
     console.log(pokemonCard);
   });
 }
-document.addEventListener('DOMContentLoaded', pokemonCards);
+//document.addEventListener('DOMContentLoaded', pokemonCards);
+document.addEventListener('DOMContentLoaded', () => {
+  pokemonCards(data.pokemon);
+});
+
+const inputElement = document.getElementById("input");
+const btnBuscar = document.getElementById("btn-buscar");
+
+btnBuscar.addEventListener("click", () => {
+  const recibeNomNum = inputElement.value.trim();
+
+  const partes = recibeNomNum.split(" ");
+  let name = "";
+  let num = "";
+  if (partes.length === 1) {
+    name = partes[0];
+  }
+  else if (partes.length === 2){
+    num = partes[0];
+    name = partes[1];
+  }
+  else {
+    console.error("Formato incorrecto. Ingresa nombre o número de Pokémon.");
+    return;
+  }
+  const busquedaInfo = busquedaNomNum(data, name, num);
+  //return pokemon.name.includes(recibeNomNum);
+  rootElement.innerHTML = "";
+  if (busquedaInfo) {
+    pokemonCards([busquedaInfo]);
+  } else {
+    console.error("El Pokémon que buscaste no está disponible");
+  }
+});
+//else{ TypeError("El pokemon que buscaste no esta disponible")
+  
+
+const btnType = document.getElementById("btn-buscarType");
+const tipoDeSeleccion = document.getElementById("types");
+
+btnType.addEventListener("click", () => {
+  //const selectType = "";
+  const seleccion = tipoDeSeleccion.value;
+  const filtrarPokemon = filtrarPorTipo(data, (pokemon) => {
+    return pokemon.type.includes(seleccion);
+  });
+  rootElement.innerHTML = "";
+  pokemonCards(filtrarPokemon);
+});
+
+
+/*const inputElement = document.getElementById("input");
+  const btnBuscar = document.getElementById("btn-buscar");
+
+  const recibeNomNum = inputElement.value;
+
+  if(recibeNomNum === "string")
+  btnBuscar.addEventListener("click", )*/

05eab80042e6d294a2c016581520f81134608560
Gaby Noriega <gabriela.noriega@alumnos.udg.mx>
2023-08-15 18:31:18 -0600

    filtro, se muestra la informacion correcta

--- src/data.js
+++ src/data.js
@@ -1,9 +1,23 @@
-// estas funciones son de ejemplo
+function filterPokemonsByType(pokemons, type) {
+    return pokemons.filter(pokemon => pokemon.type.includes(type));
+} // funcion para el primer filtro por tipo de pokemon
 
-export const example = () => {
-  return 'example';
-};
+function generateQuickMoveNames(quickMoves) {
+    return quickMoves.map(move => move.name).join(', ');
+}// extraer el nombre del string del objeto del atributo quick moves
 
-export const anotherExample = () => {
-  return 'OMG';
+function generateResistantList(resistantTypes) {
+    return resistantTypes.join(', ');
+} // extraer resistencia del objeto
+
+function generateWeaknessesList(weaknessesTypes) {
+    return weaknessesTypes.join(', ');
+} 
+
+
+export {
+    filterPokemonsByType,
+    generateQuickMoveNames,
+    generateResistantList,
+    generateWeaknessesList
 };

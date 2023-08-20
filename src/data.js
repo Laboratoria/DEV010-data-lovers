9099c8a5b59d1e2f2d62e1224fd50ee23c72959c
DianaJ-Dev <134237911+DianaJ-Dev@users.noreply.github.com>
2023-08-19 22:32:00 -0500

    Cambios css

--- src/data.js
+++ src/data.js
@@ -7,18 +7,18 @@ export const example = () => {
 
 
 // Calcula Area entre dos paises
-export const calcularArea = (data, inpuCalcular1,inpuCalcular2) =>{
+export const calcularArea = (data, inpuCalcular1, inpuCalcular2) => {
   const pais1 = data.countries.find(pais => pais.name.common === inpuCalcular1); //se busca el pais en la data ingresado
-  const pais2= data.countries.find(pais => pais.name.common === inpuCalcular2);
+  const pais2 = data.countries.find(pais => pais.name.common === inpuCalcular2);
 
-  if (pais1 != null && pais2 != null) {
+  if (pais1 !== null && pais2 !== null) {
     return pais1.area + pais2.area;
   }
-  else if (pais1===pais2){
+  else if (pais1 === pais2) {
     return pais1.area
   }
   else {
-    return('No existe alguno de los paises intente de nuevo')
+    return ('No existe alguno de los paises intente de nuevo')
   }
 };
 
@@ -27,26 +27,27 @@ export const ordenar = (banderas, tipo) => {
   const banderasClonadas = [...banderas]; // Clona las banderas existentes
   banderasClonadas.sort((a, b) => { //// Ordena las banderas clonadas por nombre de A a Z
     const nombreA = a.querySelector('h4').textContent;
-    const nombreB = b.querySelector('h4').textContent; 
-    if (tipo== "asc") {
+    const nombreB = b.querySelector('h4').textContent;
+    if (tipo === "asc") {
       return nombreA.localeCompare(nombreB);
     }
-    else if ( tipo== "dsc"){
-      return nombreA.localeCompare(nombreB)*-1;
+    else if (tipo === "dsc") {
+      return nombreA.localeCompare(nombreB) * -1;
     }
-   
+
   });
 
   return banderasClonadas;
-  
+
 };
+
 // Data por Limites 
 export const filtrarPaisesLimitantes = (data, paisElegido) => {
   const paisesLimitantes = [];
   const pais = data.countries.find(pais => pais.name.common === paisElegido); // Buscamos el país que el usuario eligió
-      if (pais) { // Si encontramos el país, buscamos sus países vecinos
+  if (pais) { // Si encontramos el país, buscamos sus países vecinos
     pais.borders.forEach(border => {
-      const paisVecino = data.countries.find(pais => pais.fifa=== border);
+      const paisVecino = data.countries.find(pais => pais.fifa === border);
       if (paisVecino) {
         paisesLimitantes.push(paisVecino);
       }
@@ -56,16 +57,16 @@ export const filtrarPaisesLimitantes = (data, paisElegido) => {
 };
 
 // Data por Idioma
-export const filtrarIdioma = (data, idiomaElegido) =>{
+export const filtrarIdioma = (data, idiomaElegido) => {
   const dataFiltradaIdioma = [];
-  for (let i=0; i<data.length; i++){
-    let lenguajes = data[i].languages;
-    for(const key in lenguajes) {
-      if(lenguajes[key] === idiomaElegido) {
+  for (let i = 0; i < data.length; i++) {
+    const lenguajes = data[i].languages;
+    for (const key in lenguajes) {
+      if (lenguajes[key] === idiomaElegido) {
         dataFiltradaIdioma.push(data[i])
       }
-    }    
+    }
   }
   return dataFiltradaIdioma;
-  
+
 }

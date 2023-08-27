e5777b8986a714cc3dcb69bc50853a85a795d0d9
DianaJ-Dev <134237911+DianaJ-Dev@users.noreply.github.com>
2023-08-27 12:38:44 -0500

    Documentado

--- src/data.js
+++ src/data.js
@@ -8,9 +8,9 @@ export const example = () => {
 
 // Calcula Area entre dos paises
 export const calcularArea = (data, inpuCalcular1, inpuCalcular2) => {
-  const pais1 = data.countries.find(pais => pais.name.common === inpuCalcular1); //se busca el pais en la data ingresado
+  const pais1 = data.countries.find(pais => pais.name.common === inpuCalcular1); //find() nos envia la primera coincidencia encontrada 
   const pais2 = data.countries.find(pais => pais.name.common === inpuCalcular2);
-  if (pais1 !== undefined && pais2 !== undefined) {
+  if (pais1 !== undefined && pais2 !== undefined) { // Se hace uso de undefined ya que deben tener valores asigandos  
     return pais1.area + pais2.area;
   }
   else if (pais1 === pais2) {
@@ -23,13 +23,13 @@ export const calcularArea = (data, inpuCalcular1, inpuCalcular2) => {
 };
 
 // Ordenar paises 
-export const ordenar = (banderas, tipo) => {
-  banderas.sort((a, b) => { //// Ordena las banderas clonadas por nombre de A a Z
+export const ordenar = (banderas, tipo) => { //Argumento (tipo) especifica el criterio por el cual se  va a ordenar  
+  banderas.sort((a, b) => { // sort ordena los elementos de un arreglo donde se le indica que a debe ir primero que b
     if (tipo === "asc") {
-      return a.name.common.localeCompare(b.name.common);
+      return a.name.common.localeCompare(b.name.common); //localeCompare compara a y b 
     }
     else if (tipo === "dsc") {
-      return -a.name.common.localeCompare(b.name.common);
+      return -a.name.common.localeCompare(b.name.common); // se coloca el - para que coloque b antes que a
     }
   });
 
@@ -40,10 +40,10 @@ export const ordenar = (banderas, tipo) => {
 // Data por Limites 
 export const filtrarPaisesLimitantes = (data, paisElegido) => {
   const paisesLimitantes = [];
-  const pais = data.countries.find(pais => pais.name.common === paisElegido); // Buscamos el país que el usuario eligió
+  const pais = data.countries.find(pais => pais.name.common === paisElegido); // find, nos envia la primera condicion encontrada 
 
   if (pais) { // Si encontramos el país, buscamos sus países vecinos
-    pais.borders.forEach(border => {
+    pais.borders.forEach(border => { // forEach ejecuta una vez cada elemento del array
       const paisVecino = data.countries.find(pais => pais.fifa === border);
       if (paisVecino) {
         paisesLimitantes.push(paisVecino);
@@ -55,12 +55,12 @@ export const filtrarPaisesLimitantes = (data, paisElegido) => {
 
 // Data por Idioma
 export const filtrarIdioma = (data, idiomaElegido) => {
-  const dataFiltradaIdioma = [];
-  for (let i = 0; i < data.length; i++) {
+  const dataFiltradaIdioma = []; // se crea un array vacio para almacenar resultados filtrados
+  for (let i = 0; i < data.length; i++) { // Intera en cada elemento de data // length se uctiliza para tener el numero total de elementos de un array
     const lenguajes = data[i].languages;
-    for (const key in lenguajes) {
+    for (const key in lenguajes) { // se define variable llamada Key la cual tomara un valor del objecto lenguaje 
       if (lenguajes[key] === idiomaElegido) {
-        dataFiltradaIdioma.push(data[i])
+        dataFiltradaIdioma.push(data[i]) // push Agrega un elemento final al arreglo que seria data [i]
       }
     }
   }
